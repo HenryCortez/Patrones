84adfb42b0f3fdbcd9fa00c9fe0f3ea5983b3b1f
Henry Cortez <henrystevencortez@yahoo.es>
2024-04-10 12:37:26 -0500

    se agrego test y maps

--- src/main/java/com/henry/cortez/factory/shapes/GeometricShapeFactory.java
+++ src/main/java/com/henry/cortez/factory/shapes/GeometricShapeFactory.java
@@ -1,18 +1,22 @@
 package com.henry.cortez.factory.shapes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GeometricShapeFactory {
+    private static final Map<String, GeometricShape> shapeMap = new HashMap<>();
+
+    static {
+        shapeMap.put("CIRCLE", new Circle());
+        shapeMap.put("SQUARE", new Square());
+        shapeMap.put("TRIANGLE", new TriangleRectangle());
+    }
+
     public static GeometricShape createShape(String shapeType, double... attributes){
         if (shapeType == null){
             return null;
         }
-        GeometricShape shape = null;
-        if (shapeType.equalsIgnoreCase("CIRCLE")){
-            shape = new Circle();
-        } else if (shapeType.equalsIgnoreCase("SQUARE")){
-            shape = new Square();
-        } else if (shapeType.equalsIgnoreCase("TRIANGLE")){
-            shape = new TriangleRectangle();
-        }
+        GeometricShape shape = shapeMap.get(shapeType.toUpperCase());
 
         if(shape != null){
             shape.setAttributes(attributes);
--- src/test/java/com/henry/cortez/factory/AppTest.java
+++ src/test/java/com/henry/cortez/factory/AppTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.henry.cortez.factory.shapes.GeometricShape;
+import com.henry.cortez.factory.shapes.GeometricShapeFactory;
+
 /**
  * Unit test for simple App.
  */
@@ -16,5 +19,31 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    @Test
+    public void Square()
+    {
+        GeometricShape square = GeometricShapeFactory.createShape("SQUARE", 5);
+        assertTrue(square.getArea() == 25);
+        assertTrue(square.getPerimeter() == 20);
+    }
+    @Test
+    public void Triangle()
+    {
+        GeometricShape triangle = GeometricShapeFactory.createShape("Triangle", 5, 10);
+        assertTrue(triangle.getArea() == 25);
+        }
+    @Test
+    public void Circle()
+    {
+        GeometricShape Circle = GeometricShapeFactory.createShape("Circle", 5);
+        assertTrue(Circle.getArea() == 78.53981633974483);
+        assertTrue(Circle.getPerimeter() == 31.41592653589793);
+    }
+    @Test
+    public void Null()
+    {
+        GeometricShape Null = GeometricShapeFactory.createShape("otra cosa", 5);
+        assertTrue(Null == null);
     }
 }
